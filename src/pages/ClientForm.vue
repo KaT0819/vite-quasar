9c2a06b79b438c8c712507c11122f523ce36188b
Katsuyuki Toki <katuyuki.toki@gmail.com>
2022-08-16 13:47:29 +0900

    validation修正

--- src/pages/ClientForm.vue
+++ src/pages/ClientForm.vue
@@ -2,11 +2,13 @@
 import { ref } from 'vue'
 import axios from 'axios'
 import UUID from 'uuidjs'
+import { useRouter } from 'vue-router'
 
 // const id = ref('');
 const code = ref<string>('')
 const name = ref<string>('')
 const email = ref<string>('')
+const router = useRouter()
 
 // 顧客
 interface Client {
@@ -25,10 +27,10 @@ const onSubmit = (event?: Event) => {
     return
   }
   if (event.target instanceof HTMLFormElement) {
-    // 顧客当億
-    postClient()
-    event.target.submit()
+    // event.target.submit()
   }
+  // 顧客当億
+  postClient()
 }
 
 const onReset = (event?: Event) => {
@@ -36,6 +38,7 @@ const onReset = (event?: Event) => {
     return
   }
   console.log('onReset', event)
+  router.push('/client')
 }
 
 // 顧客当億
@@ -57,6 +60,7 @@ const postClient = () => {
     .post(url, post.value)
     .then((response) => {
       console.log(response)
+      router.push('/client')
     })
     .then((error) => {
       console.log(error)
@@ -66,50 +70,57 @@ const postClient = () => {
 
 <template>
   <div class="q-px-md">
-    <h2>顧客情報登録</h2>
+    <h3>顧客情報登録</h3>
   </div>
-  <q-page class="flex justify-center">
+  <q-page padding class="">
     <div class="q-pa-md" style="max-width: 800px">
-      <q-form class="q-gutter-md" action="/client" @submit="onSubmit" @reset="onReset">
-        <div class="q-gutter-md row items-start">
-          <q-input
-            v-model="code"
-            filled
-            label="顧客コード *"
-            hint="数値 0-9"
-            style="min-width: 150px"
-            lazy-rules
-            :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-          />
+      <div class="q-gutter-md">
+        <q-input
+          v-model="code"
+          filled
+          label="顧客コード *"
+          hint="数値 0-9"
+          style="min-width: 150px"
+          clearable
+          :rules="[
+            (val) => !!val || '* 値を入力してください',
+            (val) => val.length <= 10 || '10文字以内で入力してください',
+          ]"
+        />
 
-          <q-input
-            v-model="name"
-            filled
-            label="顧客名 *"
-            hint="企業名、団体名など"
-            style="min-width: 150px; width: 60%"
-            lazy-rules
-            :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-          />
-        </div>
+        <q-input
+          v-model="name"
+          filled
+          label="顧客名 *"
+          hint="企業名、団体名など"
+          style="min-width: 150px; width: 60%"
+          clearable
+          :rules="[
+            (val) => !!val || '* 値を入力してください',
+            (val) => val.length <= 40 || '40文字以内で入力してください',
+          ]"
+        />
 
-        <div class="q-gutter-md row items-start">
-          <q-input
-            v-model="email"
-            filled
-            type="email"
-            size="lg"
-            label="担当者メールアドレス *"
-            hint="Email"
-            style="width: 100%"
-          />
-        </div>
-
-        <div>
-          <q-btn label="キャンセル" type="reset" size="lg" color="primary" flat class="q-ml-md" />
-          <q-btn label="登録" type="submit" color="primary" class="q-ml-md" />
-        </div>
-      </q-form>
+        <q-input
+          v-model="email"
+          filled
+          type="email"
+          size="lg"
+          label="担当者メールアドレス *"
+          hint="Email"
+          style="width: 100%"
+          clearable
+          :rules="[
+            (val) => !!val || '* 値を入力してください',
+            (val) => val.length <= 255 || '255文字以内で入力してください',
+          ]"
+        />
+      </div>
+    </div>
+    <div class="q-gutter-md">
+      <q-space />
+      <q-btn label="キャンセル" size="lg" color="primary" flat class="q-ml-md" @click="onReset" />
+      <q-btn label="登録" color="primary" class="q-ml-md" @click="onSubmit" />
     </div>
   </q-page>
 </template>
