97e9b7cde44330cf57e36867aa9a0a09119923a3
Katsuyuki Toki <katuyuki.toki@gmail.com>
2022-08-16 11:00:05 +0900

    顧客側読み込み

--- src/pages/ClientForm.vue
+++ src/pages/ClientForm.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import axios from 'axios'
+import UUID from 'uuidjs'
 
 // const id = ref('');
 const code = ref<string>('')
 const name = ref<string>('')
 const email = ref<string>('')
+
+// 顧客
+interface Client {
+  id: string
+  code: number
+  name: string
+  email: string
+}
+
+const post = ref<Client>()
 
 const onSubmit = (event?: Event) => {
   console.log('onSubmit', event)
@@ -13,6 +25,8 @@ const onSubmit = (event?: Event) => {
     return
   }
   if (event.target instanceof HTMLFormElement) {
+    // 顧客当億
+    postClient()
     event.target.submit()
   }
 }
@@ -23,6 +37,31 @@ const onReset = (event?: Event) => {
   }
   console.log('onReset', event)
 }
+
+// 顧客当億
+const postClient = () => {
+  const url = 'http://localhost:3000/clients'
+
+  // UUIDを生成
+  const id = UUID.generate()
+  console.log(id)
+
+  post.value = {
+    id: id,
+    code: Number(code.value),
+    name: name.value,
+    email: email.value,
+  }
+
+  axios
+    .post(url, post.value)
+    .then((response) => {
+      console.log(response)
+    })
+    .then((error) => {
+      console.log(error)
+    })
+}
 </script>
 
 <template>
@@ -31,7 +70,7 @@ const onReset = (event?: Event) => {
   </div>
   <q-page class="flex justify-center">
     <div class="q-pa-md" style="max-width: 800px">
-      <q-form class="q-gutter-md" @submit="onSubmit" @reset="onReset">
+      <q-form class="q-gutter-md" action="/client" @submit="onSubmit" @reset="onReset">
         <div class="q-gutter-md row items-start">
           <q-input
             v-model="code"
